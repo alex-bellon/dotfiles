3e25a147b92b22f949803f846c78c7b94f401f16
alex <alexrbellon@gmail.com>
2018-09-13 11:54:53 -0500

    Started trying to fix i3lock

--- install.sh
+++ install.sh
@@ -4,7 +4,29 @@ sudo apt-get install i3 git stow i3status zsh feh rxvt-unicode-256color tmux rof
 
 #ADD IN YES TO ALL INSTALLATION STUFF
 
-#install i3 dependencies
+cd ~/Repos
+
+#i3lock-fancy dependencies
+sudo apt-get install libev-dev libxcb-composite0 libxcb-composite0-dev libxcb-xinerama0 libxcb-randr0 libxcb-xinerama0-dev libxcb-xkb-dev libxcb-image0-dev libxcb-util-dev libxkbcommon-x11-dev libjpeg-turbo8-dev libpam0g-dev
+
+git clone https://github.com/PandorasFox/i3lock-color.git 
+
+cd i3lock-color
+
+autoreconf -i && ./configure && make
+
+#i3lock-fancy
+cd ..
+
+sudo apt-get install imagemagick util-linux
+
+git clone https://github.com/meskarune/i3lock-fancy.git
+
+cd i3lock-fancy
+
+sudo make install
+
+#i3 dependencies
 #add PPA
 #clone i3-gaps repo
 
@@ -13,3 +35,5 @@ sudo apt-get install i3 git stow i3status zsh feh rxvt-unicode-256color tmux rof
 #download zsh theme
 
 #stow everything
+
+
