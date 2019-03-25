a7ac011046a931046a8be28b89358c677f8eb29c
alex-bellon <alexrbellon@gmail.com>
2019-03-25 17:08:33 -0500

    Disabled lazy focusing

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -69,20 +69,8 @@ modkey = "Mod1"
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.floating,
-    --awful.layout.suit.tile.left,
-    --awful.layout.suit.tile.bottom,
-    --awful.layout.suit.tile.top,
     awful.layout.suit.fair,
-    --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max,
-    --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
-    --awful.layout.suit.corner.ne,
-    --awful.layout.suit.corner.sw,
-    --awful.layout.suit.corner.se,
 }
 -- }}}
 
@@ -98,7 +86,7 @@ myawesomemenu = {
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
                                     { "open terminal", terminal },
-                                    { "firefox", terminal .. "firefox" }
+                                    { "firefox", terminal.."Firefox" }
                             }
                         })
 
@@ -243,7 +231,7 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
 
-    awful.key({ modkey, }, "l", function () awful.util.spawn("lock") end ),
+    awful.key({ modkey, }, "l", function () awful.spawn.with_shell("lock") end ),
 
     -- SWITCH WORKSPACES
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
@@ -560,9 +548,9 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
+-- client.connect_signal("mouse::enter", function(c)
+--     c:emit_signal("request::activate", "mouse_enter", {raise = false})
+-- end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
